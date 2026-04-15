914c6765a3cceb7b88133d7ebfb790791309b0c2
Blaise Anyigwi <blaiseanyigwi58@gmail.com>
2026-04-15 18:35:31 -0500

    Fix Vercel API URL config

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((config) => {
