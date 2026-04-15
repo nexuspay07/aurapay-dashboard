9fd854462ed19886871a02e59acdce264ba55688
Blaise Anyigwi <blaiseanyigwi58@gmail.com>
2026-04-15 18:24:19 -0500

    Debug Vercel API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
-console.log("API URL:", import.meta.env.VITE_API_URL);
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
