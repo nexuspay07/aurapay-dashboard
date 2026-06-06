3319135769cb922c0bc1e5de955d1279fa815ea3
nexuspay07 <nexuspay07@gmail.com>
2026-06-05 21:33:36 -0500

    Fix Stripe public key integration

--- src/components/StripeWrapper.jsx
+++ src/components/StripeWrapper.jsx
@@ -2,7 +2,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 const stripePromise = loadStripe(
-  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+  import.meta.env.VITE_STRIPE_PUBLIC_KEY
 );
 
 export default function StripeWrapper({
